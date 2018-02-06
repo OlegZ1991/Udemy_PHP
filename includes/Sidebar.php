b83d7fa902974db047142767a68ff32e6317644d
Oleg Zabara <oleg__zabara@mail.ru>
2018-02-06 16:16:44 +0300

    Section 12. Lecture 94. Displaying Categories in the Sidebar

--- includes/Sidebar.php
+++ includes/Sidebar.php
@@ -19,40 +19,21 @@
                 <div class="well">
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
+                            <?php
+                                $query = "SELECT cat_title FROM category LIMIT 3";
+                                $result = mysqli_query($connect, $query);
+                                while($row = mysqli_fetch_assoc($result)):
+                                ?>
+                                <li><a href="#"><?php echo $row['cat_title'];?></a>
                                 </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+                                <?php endwhile;?>
                             </ul>
                         </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
                     </div>
                     <!-- /.row -->
                 </div>
-
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
-
+                <?php include "Widget.php";?>
             </div>
